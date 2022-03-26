48e92fa0be54af4e1ec3e1d81c13c8326576218a
Daniel Wagner <wagner.daniel87@gmail.com>
2022-03-26 17:35:07 +0100

    setup eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
 
 function AlertDismissibleExample() {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
   if (show) {
     return (
       <Alert variant="danger" onClose={() => setShow(false)} dismissible>
         <Alert.Heading>
-          I am an alert of type <span className="dangerText">danger</span>! But
-          my color is Teal!
+          I am an alert of type <span className="dangerText">danger</span>! But my color is Teal!
         </Alert.Heading>
         <p>
-          By the way the button you just clicked is an{" "}
-          <span className="infoText">Info</span> button but is using the color
-          Tomato. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Accusantium debitis deleniti distinctio impedit officia reprehenderit
-          suscipit voluptatibus. Earum, nam necessitatibus!
+          By the way the button you just clicked is an <span className="infoText">Info</span> button but is using the
+          color Tomato. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis deleniti
+          distinctio impedit officia reprehenderit suscipit voluptatibus. Earum, nam necessitatibus!
         </p>
       </Alert>
-    );
+    )
   }
   return (
     <Button variant="info" onClick={() => setShow(true)}>
       Show Custom Styled Alert
     </Button>
-  );
+  )
 }
 
 const App = () => (
@@ -39,7 +36,7 @@ const App = () => (
       <AlertDismissibleExample />
       <hr />
       <p>
-        You can check further in information on the official Bootstrap docs{" "}
+        You can check further in information on the official Bootstrap docs{' '}
         <a
           href="https://getbootstrap.com/docs/4.3/getting-started/theming/#importing"
           target="_blank"
@@ -51,6 +48,6 @@ const App = () => (
       </p>
     </Container>
   </Container>
-);
+)
 
-export default App;
+export default App

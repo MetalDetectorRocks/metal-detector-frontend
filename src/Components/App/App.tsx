0c571d455618f62aeb5b894ba1304c2dc34ec2db
Daniel Wagner <wagner.daniel87@gmail.com>
2022-06-12 15:54:58 +0200

    mui setup and basic scss definitions

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppRouter } from './router/AppRouter'
+import { AppRouter } from '../../Router/AppRouter'
 
 const App: React.FC = () => {
   return (
--- src/pages/Landing.tsx
+++
@@ -1,3 +0,0 @@
-export const LandingPage = () => {
-  return <h2>Landing Page</h2>
-}

63c9dc3111abade34765c77af9a6d8ff12e0d06f
Daniel Wagner <wagner.daniel87@gmail.com>
2022-06-12 17:25:05 +0200

    basic theme for mui components

---
+++ src/Theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material'
+
+export default createTheme({
+  palette: {
+    primary: {
+      main: '#0C0E14',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+})

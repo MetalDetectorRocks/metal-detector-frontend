94e04584c03a154734121afcece7232733b0d8c3
Nils Dammenhayn <nilsdam@aol.com>
2022-06-25 20:03:11 +0200

    Error handling

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, TextField} from "@mui/material";
 import useAxios from "axios-hooks";
-import {AxiosResponse} from "axios";
+import {AxiosError, AxiosResponse} from "axios";
 import {useCookies} from "react-cookie";
 
 export interface LoginResponse {
@@ -10,48 +10,72 @@ export interface LoginResponse {
   readonly roles: string[];
 }
 
+export interface ErrorResponse {
+  readonly timestamp: Date;
+  readonly status: number;
+  readonly error: string;
+  readonly messages: string[];
+}
+
 export const Login = () => {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [, executePost] = useAxios(
     {
       url: "/rest/v1/login",
       method: "POST"
     }
-  )
+  );
   const [, setCookie,] = useCookies(["Authorization"]);
 
-  const onClickLogin = (event: React.SyntheticEvent) => {
+  const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
-    const target = event.target as typeof event.target & {
-      email: { value: string };
-      password: { value: string };
-    }
+    setErrorMessage("");
 
     executePost({
       data: {
-        email: target.email.value,
-        password: target.password.value
+        email: email,
+        password: password
       }
-    }).then((response: AxiosResponse<LoginResponse>) => {
-      setCookie("Authorization", `Bearer ${response.data.token}`, {
-        path: "/", // ToDo: this correct?
-        maxAge: 300,
-        domain: "localhost", // ToDo: change later
-        secure: false, // ToDo: change later
-        // httpOnly: true, // ToDo: can only be set by server...
-        sameSite: "lax" // ToDo: this correct?
+    })
+      .then((response: AxiosResponse<LoginResponse>) => {
+        setCookie("Authorization", `Bearer ${response.data.token}`, {
+          path: "/", // ToDo: this correct?
+          maxAge: 300,
+          domain: "localhost", // ToDo: change later
+          secure: false, // ToDo: change later
+          // httpOnly: true, // ToDo: can only be set by server...
+          sameSite: "lax" // ToDo: this correct?
+        });
+      })
+      .catch((error: AxiosError<ErrorResponse>) => {
+        setErrorMessage(error.response?.data.messages.join(" ") as string);
       });
-    });
+  }
+
+  const handleEmailValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+
+  const renderErrorMessage = () => {
+    return <div>{errorMessage}</div>;
   }
 
   return (
     <>
       <h2>Login</h2>
-      <form onSubmit={onClickLogin.bind(this)}>
-        <TextField type="email" name="email" label="Username"/>
-        <TextField type="password" name="password" label="Password"/>
+      <form onSubmit={handleSubmit.bind(this)}>
+        <TextField type="email" name="email" label="Username" onChange={handleEmailValueChange.bind(this)} required autoFocus/>
+        <TextField type="password" name="password" label="Password" onChange={handlePasswordValueChange.bind(this)} required/>
+        {renderErrorMessage()}
         <Button type="submit">Login</Button>
       </form>
     </>
-  )
+  );
 }

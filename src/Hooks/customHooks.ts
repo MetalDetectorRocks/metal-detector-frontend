1fd8409a61f7a2500e80d55baf4c04d64e403254
Nils Dammenhayn <nilsdam@aol.com>
2022-09-27 18:38:35 +0200

    Add return type

--- src/Hooks/customHooks.ts
+++ src/Hooks/customHooks.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 
-function useDaysJs() {
+function useDaysJs(): string[] {
   const inOneMonth = dayjs().add(1, 'month').format(DateFormat.UTC)
   const oneMonthAgo = dayjs().subtract(1, 'month').format(DateFormat.UTC)
   const today = dayjs().format(DateFormat.UTC)

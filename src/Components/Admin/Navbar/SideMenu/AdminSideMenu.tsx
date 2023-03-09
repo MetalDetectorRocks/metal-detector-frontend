83637eab400c948dff8f00bbbd9bcb5dbeb366b2
Daniel Wagner <wagner.daniel87@gmail.com>
2023-03-09 16:44:56 +0100

    single component for navigation items to increase reusability

--- src/Components/Admin/Navbar/SideMenu/AdminSideMenu.tsx
+++ src/Components/Admin/Navbar/SideMenu/AdminSideMenu.tsx
@@ -4,7 +4,7 @@ import DashboardIcon from '@mui/icons-material/Dashboard'
 import GroupIcon from '@mui/icons-material/Group'
 import AlbumIcon from '@mui/icons-material/Album'
 import ImportExportIcon from '@mui/icons-material/ImportExport'
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
+import Notifications from '@mui/icons-material/Notifications'
 import DoDisturbIcon from '@mui/icons-material/DoDisturb'
 import {
   adminDashboard,
@@ -14,7 +14,7 @@ import {
   adminReleasesList,
   adminUsersList,
 } from '../../../../Router/InternalRoutes'
-import SideMenuItem from './SideMenuItem'
+import NavItem from '../../../Common/NavItem/NavItem'
 
 const AdminSideMenu = () => {
   return (
@@ -22,38 +22,38 @@ const AdminSideMenu = () => {
       <Toolbar />
       <Divider />
       <List className={classes['side-menu']}>
-        <SideMenuItem
+        <NavItem
           name={adminDashboard.name}
           path={adminDashboard.path}
           icon={<DashboardIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
+        ></NavItem>
         <h2 className={classes['side-menu__section-heading']}>Releases</h2>
-        <SideMenuItem
+        <NavItem
           name={adminReleasesList.name}
           path={adminReleasesList.path}
           icon={<AlbumIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
-        <SideMenuItem
+        ></NavItem>
+        <NavItem
           name={adminReleaseImport.name}
           path={adminReleaseImport.path}
           icon={<ImportExportIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
-        <SideMenuItem
+        ></NavItem>
+        <NavItem
           name={adminReleaseDenyList.name}
           path={adminReleaseDenyList.path}
           icon={<DoDisturbIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
+        ></NavItem>
         <h2 className={classes['side-menu__section-heading']}>Users</h2>
-        <SideMenuItem
+        <NavItem
           name={adminUsersList.name}
           path={adminUsersList.path}
           icon={<GroupIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
-        <SideMenuItem
+        ></NavItem>
+        <NavItem
           name={adminNotifications.name}
           path={adminNotifications.path}
-          icon={<NotificationsActiveIcon color={'secondary'} fontSize={'small'} />}
-        ></SideMenuItem>
+          icon={<Notifications fontSize="small" color={'secondary'} />}
+        ></NavItem>
       </List>
     </Drawer>
   )

21871b2800a89e5cf9bfb3e52eefe8f789d2c4b4
Nils Dammenhayn <nilsdam@aol.com>
2023-04-18 10:15:44 +0200

    Get back to main page from admin area

--- src/Components/Admin/Navbar/TopMenu/AdminTopMenu.tsx
+++ src/Components/Admin/Navbar/TopMenu/AdminTopMenu.tsx
@@ -2,14 +2,14 @@ import { AppBar, Toolbar } from '@mui/material'
 import logo from '../../../../assets/img/logo.png'
 import classes from './AdminTopMenu.module.scss'
 import Box from '@mui/material/Box'
-import { adminDashboard } from '../../../../Router/InternalRoutes'
+import { home } from '../../../../Router/InternalRoutes'
 import { Link } from 'react-router-dom'
 
 const AdminTopMenu = () => {
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} className={classes['app-bar']}>
       <Toolbar>
-        <Link to={adminDashboard.path}>
+        <Link to={home.path}>
           <Box component={'img'} alt={'Metal Detector Logo'} src={logo} className={classes['app-bar__logo']} />
         </Link>
       </Toolbar>

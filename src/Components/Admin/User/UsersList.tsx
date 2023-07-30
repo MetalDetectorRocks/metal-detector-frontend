b077e8d1092aba6a4d1b984fdbf1c618217b64ab
Daniel Wagner <daniel.wagner@zeb-applied.com>
2023-07-30 16:41:36 +0200

    add table actions

--- src/Components/Admin/User/UsersList.tsx
+++ src/Components/Admin/User/UsersList.tsx
@@ -3,6 +3,8 @@ import DataTable from '../../Common/Table/DataTable'
 import { columns, data, User } from '../Mock/usersTable'
 import { Alignment } from 'react-data-table-component'
 import DataTableSearch from '../../Common/Table/DataTableSearch'
+import Button from '@mui/material/Button'
+import classes from './UsersList.module.scss'
 
 const UsersList = () => {
   // const [selectedRows, setSelectedRows] = useState([])
@@ -18,19 +20,27 @@ const UsersList = () => {
       (user.email && user.email.toLowerCase().includes(searchText.toLowerCase())),
   )
 
+  const subHeaderComponent = (
+    <div className={classes['sub-header']}>
+      <DataTableSearch
+        searchText={searchText}
+        onSearch={(event: ChangeEvent<HTMLInputElement>) => setSearchText(event.target.value)}
+      />
+      <div className={classes['sub-header__actions']}>
+        <Button variant="outlined">Create Administrator</Button>
+        <Button variant="outlined">Run cleanup</Button>
+      </div>
+    </div>
+  )
+
   return (
     <>
       <DataTable
         columns={columns}
         data={filteredUsers}
         subHeader
-        subHeaderAlign={Alignment.RIGHT}
-        subHeaderComponent={
-          <DataTableSearch
-            searchText={searchText}
-            onSearch={(event: ChangeEvent<HTMLInputElement>) => setSearchText(event.target.value)}
-          />
-        }
+        subHeaderAlign={Alignment.LEFT}
+        subHeaderComponent={subHeaderComponent}
       />
     </>
   )

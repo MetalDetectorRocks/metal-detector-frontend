1e6ef5f01f8b0aeead064f1b745f4acd28af9b4e
Nils Dammenhayn <nilsdam@aol.com>
2023-11-12 17:19:31 +0100

    review: move switch styling to scss overrides

--- src/Components/NotificationSettings/NotificationSettingOptions.tsx
+++ src/Components/NotificationSettings/NotificationSettingOptions.tsx
@@ -1,14 +1,7 @@
 import classes from './NotificationSettingOptions.module.scss'
 import { FormControlLabel, Radio, RadioGroup, Switch } from '@mui/material'
-import { styled } from '@mui/system'
 
 const NotificationSettingOptions = () => {
-  const StyledSwitch = styled(Switch)(() => ({
-    '& .MuiSwitch-track': {
-      backgroundColor: 'white',
-    },
-  }))
-
   return (
     <>
       <div className={classes['notification-section']}>
@@ -34,17 +27,14 @@ const NotificationSettingOptions = () => {
       <div className={classes['notification-section']}>
         <h4 className={classes['notification-section__heading']}>Extra notifications</h4>
         <RadioGroup>
-          <FormControlLabel control={<StyledSwitch color={'info'} />} label={'Notification on release date'} />
-          <FormControlLabel control={<StyledSwitch color={'info'} />} label={'Notification on announcement date'} />
+          <FormControlLabel control={<Switch color={'info'} />} label={'Notification on release date'} />
+          <FormControlLabel control={<Switch color={'info'} />} label={'Notification on announcement date'} />
         </RadioGroup>
       </div>
       <div className={classes['notification-section']}>
         <h4 className={classes['notification-section__heading']}>Reissue notifications</h4>
         <RadioGroup>
-          <FormControlLabel
-            control={<StyledSwitch color={'info'} />}
-            label={'Notifications for reissues or re-releases'}
-          />
+          <FormControlLabel control={<Switch color={'info'} />} label={'Notifications for reissues or re-releases'} />
         </RadioGroup>
       </div>
     </>

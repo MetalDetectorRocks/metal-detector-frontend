3ff97c8a6beb5347aa9fca73410cad737a79df74
Nils Dammenhayn <nilsdam@aol.com>
2024-05-26 10:26:38 +0200

    remove web-vitals (unused) (#1321)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Components/App/App'
-import reportWebVitals from './reportWebVitals'
 import './Styles/main.scss'
 import { StyledEngineProvider, ThemeProvider } from '@mui/material'
 import { darkTheme } from './Theme'
@@ -32,8 +31,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()

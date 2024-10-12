cf76f018f23ceacd77212895d7425c0adf2b0c3b
Nils Dammenhayn <nilsdam@aol.com>
2024-10-12 12:35:48 +0200

    really fix font in docker image

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
+import * as path from 'node:path'
 
 export default defineConfig(() => {
   return {
-    base: '',
+    base: '/',
     server: {
       port: 3000,
     },
@@ -30,5 +31,10 @@ export default defineConfig(() => {
         include: ['src/**/*.svg'],
       }),
     ],
+    resolve: {
+      alias: {
+        '@fonts': path.resolve(__dirname, './src/Styles/fonts'),
+      },
+    },
   }
 })
